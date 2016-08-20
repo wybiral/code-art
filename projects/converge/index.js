9cefa992cbd18a8b151a7683531ba60520017742
wybiral <davy.wybiral@gmail.com>
2016-08-20 18:18:07 -0500

    Added create() function

--- projects/converge/index.js
+++ projects/converge/index.js
@@ -9,3 +9,20 @@ window.onload = function() {
     ctx.fillStyle = 'rgb(255,255,255)';
     ctx.fillRect(0, 0, width, height);
 };
+
+function create() {
+    const r = Math.random() * 50 + 300,
+    const a = Math.random() * Math.PI * 2;
+    const x = 350 + Math.cos(a) * r;
+    const y = 350 + Math.sin(a) * r;
+    return {
+        x: x,
+        y: y,
+        xv: Math.random() * 0.02 - 0.01,
+        yv: Math.random() * 0.02 - 0.01,
+        mass: 0.01,
+        r: 0,
+        g: 0,
+        b: 0
+    };
+}
