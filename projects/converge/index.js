723f2a6d1546b1906046e111f1fa3bda14b943b7
wybiral <davy.wybiral@gmail.com>
2016-08-20 18:15:09 -0500

    Setup canvas

--- projects/converge/index.js
+++ projects/converge/index.js
@@ -1,2 +1,11 @@
 window.onload = function() {
+    const width = 700;
+    const height = 700;
+    let canvas = document.createElement('canvas');
+    canvas.width = width;
+    canvas.height = height;
+    document.body.appendChild(canvas);
+    let ctx = canvas.getContext('2d');
+    ctx.fillStyle = 'rgb(255,255,255)';
+    ctx.fillRect(0, 0, width, height);
 };
