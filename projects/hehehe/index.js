6950321bf0d64bae24bea2460341397e5d1c12ea
davy <davy.wybiral@gmail.com>
2019-04-25 09:51:08 -0500

    inline video

--- projects/hehehe/index.js
+++ projects/hehehe/index.js
@@ -8,22 +8,15 @@ window.onload = () => {
     // Create hidden canvas
     const w = 32;
     const h = 32;
-    const canvas = document.createElement('canvas');
-    canvas.style = 'display: none';
+    const canvas = document.getElementById('canvas');
     canvas.width = w;
     canvas.height = h;
-    document.body.appendChild(canvas);
     // Grab canvas context
     const ctx = canvas.getContext('2d');
     // Create hidden video element
-    const video = document.createElement('video');
-    video.style = 'display: none';
-    video.width = canvas.width;
-    video.height = canvas.height;
-    document.body.appendChild(video);
-    // Assign src to sweet music video and start loop
-    video.src = "./rickroll.mp4";
-    video.autoplay = true;
+    const video = document.getElementById('video');
+    video.width = w;
+    video.height = h;
     // Loop forever
     const loop = () => {
         // Copy video to canvas
@@ -35,5 +28,6 @@ window.onload = () => {
     };
     loop();
     document.body.onclick = () => video.play();
+    document.body.onfocus = () => video.play();
 };
 
